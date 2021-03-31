e93036037feb4e96b7853e26cb9357388780910e
BruceMaa <BruceMaa@users.noreply.github.com>
2021-03-31 01:51:51 +0000

    Auto publish: getting started with Hugo Publish on 3

--- docs/en.search.min.2b5a55410a108528c116803e89a94b57d9c2355a443cd4f28aea102d63d028f5.js
+++ docs/en.search.min.2b5a55410a108528c116803e89a94b57d9c2355a443cd4f28aea102d63d028f5.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.f960d7f649b6e155b8e2869c7a3d4e550037ca3ce009b298251a9ca7fc17ae34.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.e8e9a91301e5648d92e735df78e7e6764260d5f5e11e994a42974bb8e1aaad2c.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
